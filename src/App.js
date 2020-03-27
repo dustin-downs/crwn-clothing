497287805181c90838931c61704310f22bf7874d
Dustin Downs <2736672+dustin-downs@users.noreply.github.com>
2020-03-27 19:42:11 +0800

    add header component and links to app shell

--- src/App.js
+++ src/App.js
@@ -5,10 +5,12 @@ import './App.css';
 
 import HomePage from './pages/home-page/home-page.component';
 import ShopPage from './pages/shop-page/shop-page.component';
+import Header from './components/header/header.component';
 
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route path="/shop" component={ShopPage} />
