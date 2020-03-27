84caf9d0a6ba11c96be7bb9cd7b97909df8adcb6
Dustin Downs <2736672+dustin-downs@users.noreply.github.com>
2020-03-27 20:42:23 +0800

    add sign in page, form input, and custom button components

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import './App.css';
 
 import HomePage from './pages/home-page/home-page.component';
 import ShopPage from './pages/shop-page/shop-page.component';
+import AuthPage from './pages/auth-page/auth-page.component';
 import Header from './components/header/header.component';
 
 function App() {
@@ -14,6 +15,7 @@ function App() {
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route path="/shop" component={ShopPage} />
+        <Route path="/signin" component={AuthPage} />
       </Switch>
     </div>
   );
